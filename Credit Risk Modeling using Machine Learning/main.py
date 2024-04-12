16a82f277d859ba836c10fb85c8a1bfa1aa69e1b
1abhi6 <1abhigup6@gmail.com>
2024-04-13 00:55:09 +0530

    Added code upto session 2

--- Credit Risk Modeling using Machine Learning/main.py
+++ Credit Risk Modeling using Machine Learning/main.py
@@ -47,21 +47,64 @@ for i in df1.columns:
 # Merge the two dataframes
 df = pd.merge(df1, df2, how="inner", left_on=['PROSPECTID'], right_on=['PROSPECTID'])
 
+# Check how many features are categorical
+categorical_features = []
+for i in df.columns:
+    if df[i].dtype == "object":
+        categorical_features.append(i)
 
+# Chi-square test
+for i in categorical_features[:-1]:
+    chi2, pval, _ , _ = chi2_contingency(pd.crosstab(df[i], df[categorical_features[-1]]))
+    print(i, "----", pval)
+    
+# Since all the categorical features have pval > 0.05, we will accept all
 
+# Variation inflation factor(VIF) for numerical columns
+numerical_columns = []
+for i in df.columns:
+    if df[i].dtype != "object" and i not in ["PROSPECTID", "Approved_Flag"]:
+        numerical_columns.append(i)
+        
 
+# VIF sequentially check
+vif_data = df[numerical_columns]
+total_columns = vif_data.shape[1]
+columns_to_be_kept = []
+column_index = 0
 
+for i in range(0, total_columns):
+    vif_value = variance_inflation_factor(vif_data, column_index)
+    print(column_index, "---", vif_value)
+    
+    if vif_value <= 6:
+        columns_to_be_kept.append(numerical_columns[i])
+        column_index = column_index+1
+    
+    else:
+        vif_data = vif_data.drop([numerical_columns[i]], axis=1)
+        
 
+# Check Anova for columns_to_be_kept
+from scipy.stats import f_oneway
 
+columns_to_be_kept_numerical = []
 
-
-
-
-
-
-
-
-
+for i in columns_to_be_kept:
+    a = list(df[i])
+    b = list(df['Approved_Flag'])
+    
+    group_P1 = [value for value, group in zip(a, b) if group == "P1"]
+    group_P2 = [value for value, group in zip(a, b) if group == "P2"]
+    group_P3 = [value for value, group in zip(a, b) if group == "P3"]
+    group_P4 = [value for value, group in zip(a, b) if group == "P4"]
+    
+    f_staristic, p_value = f_oneway(group_P1, group_P2, group_P3, group_P4)
+    
+    if p_value <= 0.05:
+        columns_to_be_kept_numerical.append(i)
+        
+        
 
 
 
